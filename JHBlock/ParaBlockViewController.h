e29a530c0c408c0c38c1da68a7c1f0f928c53b15
admin <you@example.com>
2017-02-17 11:28:53 +0800

    Some block 介绍 个人见解

---
+++ JHBlock/ParaBlockViewController.h
@@ -0,0 +1,13 @@
+//
+//  ParaBlockViewController.h
+//  JHBlock
+//
+//  Created by admin on 2017/2/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParaBlockViewController : UIViewController
+
+@end
--- JHBlock/ViewController.m
+++ JHBlock/ViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "ParaBlockViewController.h"
+/////看样式，和函数指针差不多。^代表block，*代表指针
+typedef int (^MySum) (int,int);
+typedef int (*FP)(int, int) ;
+/////直接定义的block
+int (^MySum1) (int,int)=^(int a,int b){return a+b;};
 
 @interface ViewController ()
 
@@ -17,7 +23,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    MySum sum1 = ^(int a , int b){
+        return a+b;
+    };
+    
+    NSLog(@"%d",sum1(1,2));
+    NSLog(@"%d",MySum1(10,20));
+
+    //////
+    [self mineFuncPointer];
+    //////
 }
+
+//////////瞅瞅在.m里面的函数指针
+
+int sum(int a, int b)
+{
+    NSLog(@"sum: a+b=%d", a + b);
+    return a+b;
+}
+void logSome()
+{
+    
+    NSLog(@"我是void型的输出函数");
+    
+}
+
+-(void)mineFuncPointer{
+    int (*p)(int, int) = sum;
+    p(2, 3);
+
+    //指向无参数的函数logSome()函数
+    void (*logP)() = logSome;
+    // 调用指向函数的指针函数
+    logP();
+    
+    ////////
+    FP pp =sum;
+    NSLog(@"FP: %d", pp(3,3));
+
+    
+    
+}
+
+//////////
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    ParaBlockViewController *para = [[ParaBlockViewController alloc]init];
+    [self.navigationController pushViewController:para animated:YES];
+}
+
 
 
 - (void)didReceiveMemoryWarning {
